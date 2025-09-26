a353f94283bb704e84ca4a9f889059686137698a
myorn <lednovalex@gmail.com>
2025-09-26 18:48:28 +0300

    And they don't stop coming

--- init.lua
+++ init.lua
@@ -300,8 +300,39 @@ require('lazy').setup({
     },
   },
 
+  {
+    'nvimdev/indentmini.nvim',
+    config = function()
+      require('indentmini').setup()
+    end,
+  },
+
+  {
+    'yuukiflow/Arduino-Nvim',
+    config = function()
+      -- Load LSP configuration first
+      -- require('Arduino-Nvim').setup()
+
+      -- Set up Arduino file type detection
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'arduino',
+        callback = function()
+          require 'Arduino-Nvim'
+        end,
+      })
+    end,
+  },
+
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  -- 'NMAC427/guess-indent.nvim',
+  --
+  { -- Detect tabstop and shiftwidth automatically
+    "nmac427/guess-indent.nvim",
+    config = function()
+      require("guess-indent").setup({})
+    end,
+    lazy = false, -- or event = "BufReadPre" (make sure it gets loaded)
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -493,7 +524,13 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep {
+          additional_args = function()
+            return { '-j1' }
+          end,
+        }
+      end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
